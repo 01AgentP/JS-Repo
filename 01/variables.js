2814b67ccf2a19e37748c31cc3032ed812dce3ef
01AgentP <138208254+01AgentP@users.noreply.github.com>
2025-02-09 07:08:14 +0000

    variables class

---
+++ 01/variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553;
+let accountEmail = 'pankajsharma771017@gmail.com';
+var accountPassword = '1212@1212';
+console.table([accountEmail,accountId,accountPassword]);
+
+// Prefer not to use var
+// because of issue in block scope
